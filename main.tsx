17df2fa768a37e0e245981db0268429ba4adaa7f
khorbiedetechk-afk <khorbiedetechk@gmail.com>
2026-04-03 15:52:02 +0100

    Delete main.tsx

--- main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import SmartCartCampus from "./components/SmartCartCampus";
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <SmartCartCampus />
-  </StrictMode>
-);
